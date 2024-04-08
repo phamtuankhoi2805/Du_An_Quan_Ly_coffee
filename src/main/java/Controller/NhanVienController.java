ca67ba38122339a9c9c435d4eddb95483b78c2ba
KHOIDEV <kscc020@gmail.com>
2024-04-08 20:32:50 +0700

    Update from NhanVien vs2.0

--- src/main/java/Controller/NhanVienController.java
+++ src/main/java/Controller/NhanVienController.java
@@ -2,6 +2,7 @@ package Controller;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
@@ -9,19 +10,35 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.JTextField;
+import javax.swing.SpinnerModel;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
+import com.graphbuilder.math.DivNode;
+
+import DAO.KhuyenMaiDAO;
 import DAO.SanPhamDAO;
+import Model.KhuyenMaiModel;
+import Model.SanPhamHDModel;
 import Model.SanPhamModel;
 import View.NhanVienView;
 import View.TestSP;
@@ -30,7 +47,8 @@ public class NhanVienController implements ActionListener {
 
 	private NhanVienView nvv;
 	private SanPhamModel spm;
-
+	ArrayList<SanPhamHDModel> listSPHD = new ArrayList<SanPhamHDModel>();
+	private double khuyenMaii = 0.0;
 	public NhanVienController(NhanVienView nvv, SanPhamModel spm) {
 
 		this.nvv = nvv;
@@ -100,16 +118,12 @@ public class NhanVienController implements ActionListener {
 		lbl_giaSP1.setBounds(76, 61, 66, 22);
 		panel_SP.add(lbl_giaSP1);
 
-		JSpinner spinner = new JSpinner();
-		spinner.setModel(new SpinnerNumberModel(Integer.valueOf(1), Integer.valueOf(1), null, Integer.valueOf(1)));
-		spinner.setBounds(236, 63, 30, 20);
-		panel_SP.add(spinner);
-
 		panel_SP.addMouseListener(new MouseAdapter() {
+
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				// Xử lý sự kiện khi nhấp chuột vào sản phẩm
-				handleSanPhamClick(sp, spinner);
+
+				handleSanPhamClick(sp);
 			}
 
 		});
@@ -117,12 +131,38 @@ public class NhanVienController implements ActionListener {
 		return panel_SP;
 	}
 
-	public void handleSanPhamClick(SanPhamModel sp, JSpinner spinner) {
-		// Xử lý sự kiện khi nhấp chuột vào sản phẩm
-		System.out.println("Tên sản phẩm: " + sp.getTenSP());
-		System.out.println("Giá: " + sp.getGiaBan());
-		int spinnerValue = (int) spinner.getValue();
-		System.out.println("Giá trị của Spinner: " + spinnerValue);
+	public void handleSanPhamClick(SanPhamModel sp) {
+		String tenSP = sp.getTenSP();
+		String idSP = sp.getIdSanPham();
+		double giaBan = sp.getGiaBan();
+		String hinh = sp.getHinh();
+		String loaiSP = sp.getLoaiSP();
+		int soLuong = 1;
+		SanPhamHDModel spmoi = new SanPhamHDModel(idSP, tenSP, giaBan, soLuong, hinh, loaiSP);
+
+		// Kiểm tra xem sản phẩm đã có Id trong danh sách hay chưa
+		boolean isExisting = false;
+		for (SanPhamHDModel existingSP : listSPHD) {
+			if (existingSP.getIdSanPham().equals(idSP)) {
+				isExisting = true;
+				break;
+			}
+		}
+
+		if (!isExisting) {
+			listSPHD.add(spmoi);
+//			System.out.println(listSPHD.toString());
+			String totalPrice = calculateTotalPrice(listSPHD);
+			nvv.lbl_tongCong().setText(totalPrice+" VND");
+			setKhuyenMai(totalPrice);
+		
+			double tongCong = Double.parseDouble(totalPrice);
+
+			nvv.lbl_tongTien().setText(Double.toString(tongCong-khuyenMaii)+"00 VND");
+			loadProductsToScrollPane(listSPHD);
+		} else {
+			JOptionPane.showMessageDialog(nvv, "sản phẩm đã tồn tại trong danh sách");
+		}
 	}
 
 	private void applyBottomBorder(JTextField textField) {
@@ -158,5 +198,189 @@ public class NhanVienController implements ActionListener {
 		// Thiết lập ảnh cho JLabel
 		ImageIcon resizedImageIcon = new ImageIcon(scaledImage);
 		label.setIcon(resizedImageIcon);
+	}
+
+	public void loadProductsToScrollPane(ArrayList<SanPhamHDModel> ListSPHD) {
+	    nvv.panelcon().removeAll();
+	    for (SanPhamHDModel sp : ListSPHD) {
+	        JPanel panel_SP = new JPanel();
+	        panel_SP.setLayout(null);
+	        panel_SP.setBorder(createGrayBorder());
+	        panel_SP.setPreferredSize(new Dimension(380, 84));
+	        nvv.panelcon().add(panel_SP);
+
+	        JLabel lb_anhSP = new JLabel("");
+	        lb_anhSP.setBounds(10, 11, 56, 72);
+	        panel_SP.add(lb_anhSP);
+	        setScaledImage(lb_anhSP, sp.getHinh());
+
+	        JLabel lbl_tenSP = new JLabel(sp.getTenSP());
+	        lbl_tenSP.setFont(new Font("Tahoma", Font.PLAIN, 16));
+	        lbl_tenSP.setBounds(75, 11, 190, 22);
+	        panel_SP.add(lbl_tenSP);
+
+	        JLabel lbl_giaSP = new JLabel(String.valueOf(sp.getGiaBan()));
+	        lbl_giaSP.setFont(new Font("Tahoma", Font.PLAIN, 16));
+	        lbl_giaSP.setBounds(76, 61, 66, 22);
+	        panel_SP.add(lbl_giaSP);
+
+	        JLabel lbl_soLuong = new JLabel("Số Lượng: ");
+	        lbl_soLuong.setFont(new Font("Tahoma", Font.PLAIN, 16));
+	        lbl_soLuong.setBounds(170, 61, 100, 20);
+	        panel_SP.add(lbl_soLuong);
+
+	        SpinnerNumberModel spinnerModel = new SpinnerNumberModel(sp.getSoLuong(), 1, 100, 1);
+	        JSpinner spinner = new JSpinner(spinnerModel);
+	        spinner.setFont(new Font("Tahoma", Font.PLAIN, 16));
+	        spinner.setBounds(250, 58, 60, 26);
+	        panel_SP.add(spinner);
+	        spinner.addChangeListener(new ChangeListener() {
+	            @Override
+	            public void stateChanged(ChangeEvent e) {
+	                JSpinner source = (JSpinner) e.getSource();
+	                SpinnerModel spinnerModel = source.getModel();
+	                int quantity = (int) spinnerModel.getValue();
+
+	                sp.setSoLuong(quantity);
+
+	                String totalPrice = calculateTotalPrice(ListSPHD);
+	                double tongCong = Double.parseDouble(totalPrice);
+
+	                setKhuyenMai(totalPrice);
+	                nvv.lbl_tongCong().setText(totalPrice + " VND");
+
+	                nvv.lbl_tongTien().setText(Double.toString(tongCong - khuyenMaii) + "00 VND");
+
+	                if (khuyenMaii == 0.0) {
+	                    nvv.lbl_khuyenMai().setText("0 VND");
+	                } else {
+	                    String t = Double.toString(khuyenMaii) + "00";
+	                    nvv.lbl_khuyenMai().setText(t + " VND");
+	                }
+
+	                loadProductsToScrollPane(ListSPHD);
+	            }
+	        });
+
+	        JButton btn_xoaSP = new JButton("");
+	        btn_xoaSP.setBackground(new Color(255, 255, 255));
+	        btn_xoaSP.setIcon(new ImageIcon("C:\\javvaa\\DuAn1.2\\src\\main\\resources\\View\\ThungRac.png"));
+	        btn_xoaSP.setBounds(330, 52, 46, 33);
+	        panel_SP.add(btn_xoaSP);
+
+	        btn_xoaSP.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	                JPanel parentPanel = (JPanel) btn_xoaSP.getParent();
+	                int index = nvv.panelcon().getComponentZOrder(parentPanel);
+	                ListSPHD.remove(index);
+
+	                if (ListSPHD.isEmpty()) {
+	                    khuyenMaii = 0;
+	                    nvv.lbl_khuyenMai().setText("0 VND");
+	                }
+
+	                String totalPrice = calculateTotalPrice(ListSPHD);
+	                double tongCong = Double.parseDouble(totalPrice);
+
+	                setKhuyenMai(totalPrice);
+	                nvv.lbl_tongCong().setText(totalPrice + " VND");
+
+	                nvv.lbl_tongTien().setText(Double.toString(tongCong - khuyenMaii) + "00 VND");
+
+	                if (khuyenMaii == 0.0) {
+	                    nvv.lbl_khuyenMai().setText("0 VND");
+	                } else {
+	                    String t = Double.toString(khuyenMaii) + "00";
+	                    nvv.lbl_khuyenMai().setText(t + " VND");
+	                }
+
+	                loadProductsToScrollPane(ListSPHD);
+	            }
+	        });
+
+	        nvv.panelcon().add(panel_SP);
+	    }
+
+	    nvv.panelcon().revalidate();
+	    nvv.panelcon().repaint();
+	    nvv.JScrollPane().setViewportView(nvv.panelcon());
+	}
+	public void tinhTongTien() {
+//	    double tongTien = tongCong - khuyenMaii;
+//	    return tongTien;
+		System.out.println(khuyenMaii);
+	}
+	public String calculateTotalPrice(ArrayList<SanPhamHDModel> listDSHD) {
+	    double totalPrice = 0.0;
+
+	    for (SanPhamHDModel sp : listDSHD) {
+	        int quantity = sp.getSoLuong();
+	        double price = sp.getGiaBan();
+	        totalPrice += quantity * price;
+	    }
+
+	    long roundedTotalPrice = Math.round(totalPrice);
+	    DecimalFormat decimalFormat = new DecimalFormat("#,###");
+	    String formattedTotalPrice = decimalFormat.format(roundedTotalPrice);
+
+	    return formattedTotalPrice;
+	}
+	public void setKhuyenMai(String gia) {
+	    ArrayList<KhuyenMaiModel> listKM = KhuyenMaiDAO.getInstance().selectAll();
+	    double giaCanTru = Double.parseDouble(gia);
+
+	    double closestPrice = findClosestPrice(listKM, giaCanTru);
+	    boolean khuyenMaiUpdated = false;
+
+	    for (KhuyenMaiModel kmm : listKM) {
+	        String input = kmm.getDieuKienKM();
+	        String regex = "\\d+(\\.\\d+)?";
+
+	        Pattern pattern = Pattern.compile(regex);
+	        Matcher matcher = pattern.matcher(input);
+	        while (matcher.find()) {
+	            String numberString = matcher.group();
+	            double number = Double.parseDouble(numberString);
+	            if (number == closestPrice) {
+	                double km = Math.floor(giaCanTru * kmm.getTongTru());
+	                khuyenMaii = km;
+
+	                String t = Double.toString(km) + "00";
+	                nvv.lbl_khuyenMai().setText(t + " VND");
+
+	                khuyenMaiUpdated = true;
+	            }
+	        }
+	    }
+
+	    if (!khuyenMaiUpdated) {
+	        khuyenMaii = 0.0;
+	        nvv.lbl_khuyenMai().setText("0 VND");
+	    }
+	}
+	private String formatCurrency(double amount) {
+	    DecimalFormat decimalFormat = new DecimalFormat("#,### VND");
+	    return decimalFormat.format(amount);
+	}
+	private double findClosestPrice(ArrayList<KhuyenMaiModel> list, double targetPrice) {
+	    double closestPrice = Double.MAX_VALUE;
+	    double minDifference = Double.MAX_VALUE;
+
+	    for (KhuyenMaiModel kmm : list) {
+	        double price = extractNumber(kmm.getDieuKienKM());
+	        double difference = Math.abs(targetPrice - price);
+	        if (difference < minDifference) {
+	            closestPrice = price;
+	            minDifference = difference;
+	        }
+	    }
+
+	    return closestPrice;
+	}
+
+	private double extractNumber(String input) {
+	    String numberString = input.replaceAll("[^\\d.]", "");
+	    return Double.parseDouble(numberString);
 	}
 }
--- src/main/java/View/NhanVienView.java
+++ src/main/java/View/NhanVienView.java
@@ -46,8 +46,14 @@ public class NhanVienView extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-   private ArrayList<SanPhamModel> listSP;
+	private ArrayList<SanPhamModel> listSP;
 	private JPanel panel_DSSP;
+	private JPanel panelCon;
+	private JScrollPane scrollPane;
+	private JLabel lbl_tongCong;
+	private JLabel lbl_khuyenMai;
+	private JLabel lbl_tongTien;
+
 	/**
 	 * Launch the application.
 	 */
@@ -68,9 +74,9 @@ public class NhanVienView extends JFrame {
 	 * Create the frame.
 	 */
 	public NhanVienView() {
-		
-		  listSP = SanPhamDAO.getInstance().selectAll();
-		NhanVienController nvc =  new NhanVienController(this, null);
+
+		listSP = SanPhamDAO.getInstance().selectAll();
+		NhanVienController nvc = new NhanVienController(this, null);
 		setTitle("Bán Hàng");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1320, 718);
@@ -90,8 +96,6 @@ public class NhanVienView extends JFrame {
 		layeredPane.add(panel);
 		panel.setLayout(null);
 		panel.setBorder(createGrayBorder());
-
-	
 
 		JLabel lblNewLabel_3 = new JLabel("Tổng Cộng");
 		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 16));
@@ -116,23 +120,23 @@ public class NhanVienView extends JFrame {
 		separator_1_1.setBounds(10, 565, 390, 2);
 		panel.add(separator_1_1);
 
-		JLabel lblNewLabel_3_3 = new JLabel("25000");
-		lblNewLabel_3_3.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel_3_3.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNewLabel_3_3.setBounds(303, 502, 97, 26);
-		panel.add(lblNewLabel_3_3);
+		 lbl_tongCong = new JLabel("0 VND");
+		lbl_tongCong.setHorizontalAlignment(SwingConstants.RIGHT);
+		lbl_tongCong.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lbl_tongCong.setBounds(120, 497, 277, 26);
+		panel.add(lbl_tongCong);
 
-		JLabel lblNewLabel_3_3_1 = new JLabel("-5000");
-		lblNewLabel_3_3_1.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel_3_3_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNewLabel_3_3_1.setBounds(303, 539, 97, 26);
-		panel.add(lblNewLabel_3_3_1);
+		 lbl_khuyenMai = new JLabel("0 VND");
+		lbl_khuyenMai.setHorizontalAlignment(SwingConstants.RIGHT);
+		lbl_khuyenMai.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lbl_khuyenMai.setBounds(117, 539, 283, 26);
+		panel.add(lbl_khuyenMai);
 
-		JLabel lblNewLabel_3_3_2 = new JLabel("20000");
-		lblNewLabel_3_3_2.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel_3_3_2.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNewLabel_3_3_2.setBounds(303, 576, 97, 26);
-		panel.add(lblNewLabel_3_3_2);
+		 lbl_tongTien = new JLabel("0 VND");
+		lbl_tongTien.setHorizontalAlignment(SwingConstants.RIGHT);
+		lbl_tongTien.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lbl_tongTien.setBounds(117, 576, 283, 26);
+		panel.add(lbl_tongTien);
 
 		JButton btnXutHan = new JButton("Xuát Hóa Đơn");
 		btnXutHan.setForeground(new Color(255, 255, 255));
@@ -156,64 +160,82 @@ public class NhanVienView extends JFrame {
 		panel_3.add(lblNewLabel);
 		lblNewLabel.setForeground(new Color(255, 255, 255));
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		
-		JScrollPane scrollPane = new JScrollPane();
+
+		scrollPane = new JScrollPane(panelCon);
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		scrollPane.setBounds(10, 64, 390, 427);
 
 		// Tạo một JPanel để chứa các panel con
-		JPanel panelCon = new JPanel();
+		panelCon = new JPanel();
 		panelCon.setLayout(new BoxLayout(panelCon, BoxLayout.Y_AXIS));
-
 
 		// Đặt panelCon làm nội dung của scrollPane
 		scrollPane.setViewportView(panelCon);
 
 		panel.add(scrollPane);
-		 panel_DSSP = new JPanel();
+
+		panel_DSSP = new JPanel();
 		panel_DSSP.setBounds(430, 88, 874, 580);
 		layeredPane.add(panel_DSSP);
 		panel_DSSP.setLayout((LayoutManager) new FlowLayout(FlowLayout.LEFT, 10, 10));
 		panel_DSSP.setBorder(createGrayBorder());
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Thể Loại");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		lblNewLabel_1.setBounds(434, 11, 96, 21);
 		layeredPane.add(lblNewLabel_1);
-		
+
 		JButton btn_All = new JButton("ALL");
 		btn_All.setForeground(Color.WHITE);
 		btn_All.setBackground(new Color(0, 0, 160));
 		btn_All.setBounds(430, 43, 111, 34);
 		layeredPane.add(btn_All);
-		
+
 		JButton btn_Coffe = new JButton("Coffee");
 		btn_Coffe.setForeground(Color.WHITE);
 		btn_Coffe.setBackground(new Color(0, 0, 160));
 		btn_Coffe.setBounds(551, 43, 111, 34);
 		layeredPane.add(btn_Coffe);
-		
+
 		JButton btn_TraiCay = new JButton("Nước Trái Cây");
 		btn_TraiCay.setForeground(Color.WHITE);
 		btn_TraiCay.setBackground(new Color(0, 0, 160));
 		btn_TraiCay.setBounds(672, 43, 122, 34);
 		layeredPane.add(btn_TraiCay);
-		
+
 		JButton btn_tangLuc = new JButton("Tăng Lực");
 		btn_tangLuc.setForeground(Color.WHITE);
 		btn_tangLuc.setBackground(new Color(0, 0, 160));
 		btn_tangLuc.setBounds(804, 43, 122, 34);
 		layeredPane.add(btn_tangLuc);
-        nvc.addSanPhamToPanel(listSP);
-		
-        btn_All.addActionListener(nvc);
-        btn_Coffe.addActionListener(nvc);
-        btn_TraiCay.addActionListener(nvc);
+		nvc.addSanPhamToPanel(listSP);
+
+		btn_All.addActionListener(nvc);
+		btn_Coffe.addActionListener(nvc);
+		btn_TraiCay.addActionListener(nvc);
 		btn_tangLuc.addActionListener(nvc);
 
 	}
-   public JPanel panel_DSSP(){
-	   return panel_DSSP;
+
+	public JPanel panel_DSSP() {
+		return panel_DSSP;
+	}
+
+	public JPanel panelcon() {
+		return panelCon;
+	}
+   public JLabel lbl_tongCong() {
+	   return lbl_tongCong;
    }
+   public JLabel lbl_khuyenMai() {
+	   return lbl_khuyenMai;
+   }
+   public JLabel lbl_tongTien() {
+	   return lbl_tongTien;
+   }
+//   public JScrollPane JScrollPane() {
+//	   return scrollPane;
+//   }
 	private void applyBottomBorder(JTextField textField) {
 		Border border = textField.getBorder();
 		Border bottomBorder = BorderFactory.createMatteBorder(0, 0, 1, 0, Color.BLACK); // Màu đen, độ dày là 1 pixel
@@ -247,5 +269,10 @@ public class NhanVienView extends JFrame {
 		// Thiết lập ảnh cho JLabel
 		ImageIcon resizedImageIcon = new ImageIcon(scaledImage);
 		label.setIcon(resizedImageIcon);
+	}
+
+	public javax.swing.JScrollPane JScrollPane() {
+		// TODO Auto-generated method stub
+		return scrollPane;
 	}
 }
